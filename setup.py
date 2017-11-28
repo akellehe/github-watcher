d60eccd312069d7cc96ab2d582310f77c504b609
Andrew Kelleher <andrew.kelleher@venmo.com>
2017-11-28 15:55:11 -0500

    Better help message

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 
 setup(name='github_watcher',
-        version='2.2',
+        version='2.3',
         description='Monitors files/directories on github and alerts you when someone submits a PR with changes',
         author='Andrew Kelleher',
         author_email='keats.kelleher@gmail.com',
